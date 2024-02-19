4e58ba2273b4c178eb73936eaf83015fa310b11c
minphone27 <minphonemyat2726@gmail.com>
2024-02-19 18:33:38 +0700

    change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,20 +5,18 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>Schedjuice</title>
-      <link rel="icon" type="image/svg+xml" href="/img/favicon.svg" />
-      <link rel="icon" type="image/png" href="/img/favicon.png"></link>
       <meta
-        name="description"
-        content="Schedjuice - Transform the way you administer education with Schedjuice"
-      />
-      <meta
-        property="og:image"
-        content="https://schedjuice.com/img/Icon-Color.png"
-      />
-      <meta
-        property="og:description"
-        content="Schedjuice - Transform the way you administer education with Schedjuice"
-      />
+  name="description"
+  content="Schedjuice - Transform the way you administer education with Schedjuice"
+/>
+<meta
+          property="og:image"
+          content="https://schedjuice.com/img/Icon-Color.png"
+        />
+         <meta
+          property="og:description"
+          content="Schedjuice - Transform the way you administer education with Schedjuice"
+        />
       <body>
         <Main />
         <NextScript />
